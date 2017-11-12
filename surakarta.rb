2ddd554c990e252201ece034b518a81a3c37ce77
gregorsharpe <gregorbsharpe@gmail.com>
2017-11-12 12:14:09 -0500

    View class added, display_board() finished.

--- surakarta.rb
+++ surakarta.rb
@@ -20,8 +20,8 @@ class Space
    	end
 
    	# returns the Player_id specified by the owner variable.
-	def occupied_by() 
-		return @player_id 
+	def occupied_by()
+		return @player_id
 	end
 end
 
@@ -29,13 +29,13 @@ class Player
 
 	# constructor
 	def initialize(id, player_name)
-		@player_id = id 
+		    @player_id = id
         @name = player_name
         @score =  0
     end
 
     # the player ends the game.
-    def quit() 
+    def quit()
 
     end
 
@@ -44,7 +44,7 @@ class Player
     	print "Does the opposing player wish to accept the current player's forfeit? 1. Yes 2. No"
     	@answer = gets
 
-    	if @answer = true 
+    	if @answer == true
     		sarakarta.end_round(true)
     	else
     		print "The opposing player refuses to forfeit."
@@ -114,6 +114,41 @@ class Player
 
     	return true
 
- 	end	
+ 	end
 
 end
+
+class View
+    def initialize()
+  		    @current_view = ['1','1','1','1','1','1','1','1','1','1','1','1','0','0','0','0','0','0','0','0','0','0','0','0','2','2','2','2','2','2','2','2','2','2','2','2']
+    end
+
+    # Uses Board's get_spaces() to update @Current_view
+    def update_spaces()
+
+    end
+
+    # Display state of the board.
+    def display_board()
+        print "+-----------+  +-----------+\n"
+        print "|  +-----+  |  |  +-----+  |\n"
+        print "|  |     |  |  |  |     |  |\n"
+        print "|  |  #{@current_view[0]}--#{@current_view[1]}--#{@current_view[2]}--#{@current_view[3]}--#{@current_view[4]}--#{@current_view[5]}  |  |\n"
+        print "|  |  |  |  |  |  |  |  |  |\n"
+        print "|  +--#{@current_view[6]}--#{@current_view[7]}--#{@current_view[8]}--#{@current_view[9]}--#{@current_view[10]}--#{@current_view[11]}--+  |\n"
+        print "|     |  |  |  |  |  |     |\n"
+        print "+-----#{@current_view[12]}--#{@current_view[13]}--#{@current_view[14]}--#{@current_view[15]}--#{@current_view[16]}--#{@current_view[17]}-----+\n"
+        print "      |  |  |  |  |  |\n"
+        print "+-----#{@current_view[18]}--#{@current_view[19]}--#{@current_view[20]}--#{@current_view[21]}--#{@current_view[22]}--#{@current_view[23]}-----+\n"
+        print "|     |  |  |  |  |  |     |\n"
+        print "|  +--#{@current_view[24]}--#{@current_view[25]}--#{@current_view[26]}--#{@current_view[27]}--#{@current_view[28]}--#{@current_view[29]}--+  |\n"
+        print "|  |  |  |  |  |  |  |  |  |\n"
+        print "|  |  #{@current_view[30]}--#{@current_view[31]}--#{@current_view[32]}--#{@current_view[33]}--#{@current_view[34]}--#{@current_view[35]}  |  |\n"
+        print "|  |     |  |  |  |     |  |\n"
+        print "|  +-----+  |  |  +-----+  |\n"
+        print "+-----------+  +-----------+\n"
+    end
+end
+
+#testview = View.new()
+#testview.display_board()

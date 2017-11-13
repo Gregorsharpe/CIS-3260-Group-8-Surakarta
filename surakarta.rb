0c2ca117da26d615233b9d17df0eb600a6f893b4
rrintjem <robyn.rintjema@gmail.com>
2017-11-12 19:29:02 -0500

    Add files via upload

--- surakarta.rb
+++ surakarta.rb
@@ -152,3 +152,102 @@ end
 
 #testview = View.new()
 #testview.display_board()
+
+class Board
+
+	#constructor
+	def initialize()
+		@spaces = Array.new(28)
+		@loop_entrances = Array.new(8)
+		
+	end
+
+	def check_Capture(src, entr) 
+		#Description: The board evaluates validity of a capture based on the Space src, and the Loop space entrance. Returns the captured space upon success and nil on failure.  
+		#check_Capture() checks the spaces in between the start Space and the loop entrance Loop_Space. 
+		#If there are pieces in between, returns nil. 
+		#check_Capture() then jumps to the Loop_Space connected to the entrance Loop_Space and gets the direction to continue checking all of the spaces belonging to that particular loop, changing the direction of the check, if it needs to check through another Loop_Space. 
+		#If no piece is encountered and check returns to its start Space, it is not a valid capture, check_Capture() returns nil.  
+		#If player's own piece in encountered, not a valid capture, check_Capture() returns nil. 
+		#If an opposing piece is encountered, capture was successful, check_Capture() returns coordinates of space being captured. 
+
+
+		#return Space
+	end
+
+	def check_Move(src, dest) 
+		valid = false  
+		
+		#ensure destination space is empty
+		if dest.occupied_by() == 0
+
+			#check that the difference between the X and Y coordinates is 1 or -1, 
+			x_diff = src.get_coordinates[0] - dest.get_coordinates[0]
+			y_diff = src.get_coordinates[1] - dest.get_coordinates[1]
+			if x_diff == 1 || x_diff == -1
+				if y_diff == 1 || y_diff == -1
+
+					#success!
+					valid = true
+				else 
+					valid = false
+				end
+			else 
+				valid = false
+			end
+		else
+			valid = false
+		end
+
+		if valid == true 
+			return dest
+		else
+			return nil
+		end
+	end
+
+	def find_space(x, y)
+		#Description: The Board will search through its list of Spaces and Loop_Spaces looking for a space with the coordinates given by the parameters. If it finds a space in this process, returns that space, otherwise returns nil. 
+		
+		#return Space
+	end
+
+	def get_Spaces()
+		#Returns information regarding every space on the board. Loop entrances inside the board, will become their space equivalents. 
+		all_spaces = new.Array.new(36) 
+		all_spaces = @spaces + @loop_entrances
+		return  all_spaces
+	end
+
+	def perform_move(src,dest)
+
+		dest.change_ownership(src.occupied_by())
+		src.change_ownership(0)
+
+	end
+end
+
+class Loop_Space < Space
+	@next_space
+	@direction
+
+	# constructor
+   	def initialize(x,y, player_id)
+      @x_pos = x
+      @y_pos = y
+      @owner = player_id
+   	end
+
+	def get_Direction()
+		return @direction
+	end
+
+	def get_next_Loop_Space() 
+		return @next_space
+	end
+
+	def set_next_Loop_Space(next_space) 
+		@next_space = next_space
+	end
+end
+
